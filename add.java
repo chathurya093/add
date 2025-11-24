deab105937ef44282091f4fb388ac20df05541d8
chathurya093 <chathurya093@gmail.com>
2025-11-24 11:30:06 +0530

    Refactor AdditionExample class to Addition
    Updated the AdditionExample class to Addition and modified variable names.

--- add.java
+++ add.java
@@ -1,8 +1,13 @@
-public class AdditionExample {
+public class Addition{
     public static void main(String[] args) {
-        int number1 = 10;
-        int number2 = 20;
+     
+        int num1 = 10;
+        int num= 25;
+
+        /
         int sum = number1 + number2;
-        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+
+     
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }
